e63b2878825e98a04ebe0488b97b4dad23f2b93a
Ramy Rahmani <Med_rahmani01@live.fr>
2023-07-08 18:57:52 +0100

    form submission

--- form/views.py
+++ form/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from django import forms
+
+class MyForm(forms.Form):
+    fullname = forms.CharField(max_length=100)
+    age = forms.IntegerField(min_value=5)
 
 def formview(request):
-    return render(request,'form.html')
+    if request.method=='POST':
+        form=MyForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['fullname']
+            age = form.cleaned_data['age']
+            x=1
+            return render(request , 'subscribed.html',{'name':name,'x':x})
+    else:
+        form=MyForm()
+    return render(request, 'form.html', {'form': form})
